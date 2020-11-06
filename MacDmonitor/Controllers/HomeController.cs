db6e0c2d5dc8295a2aa093db6f8cbb6d26d648ec
diego.moreno <diego.moreno@radialSG.com>
2020-11-06 16:59:23 +0100

    initial

---
+++ MacDmonitor/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MacDmonitor.Models;
+
+namespace MacDmonitor.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            var rec = new Operation
+            {
+                Description = "Öperation 1"
+            };
+
+            ViewBag.Operation = rec;
+
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ MacDmonitor/Models/Operation.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MacDmonitor.Models
+{
+    public class Operation
+    {
+        public string Description { get; set; }
+    }
+}
